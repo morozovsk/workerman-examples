1cd0514540d9bf90396cb0cf0389819338af8b3a
Vladimir Goncharov <morozovsk@gmail.com>
2018-01-10 19:39:57 +0200

    Update send.php

--- integration/send.php
+++ integration/send.php
@@ -3,7 +3,7 @@ $localsocket = 'tcp://127.0.0.1:1234';
 $user = 'tester01';
 $message = 'test';
 
-// соединяемся с локальным tcp-сервером
+// connect to a local tcp-server
 $instance = stream_socket_client($localsocket);
-// отправляем сообщение
+// send message
 fwrite($instance, json_encode(['user' => $user, 'message' => $message])  . "\n");
