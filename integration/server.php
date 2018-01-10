9b3b62089953f22728b22891eef69af0ceea174d
Vladimir Goncharov <morozovsk@gmail.com>
2018-01-10 20:45:42 +0200

    Update server.php

--- integration/server.php
+++ integration/server.php
@@ -2,44 +2,46 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use Workerman\Worker;
 
-// массив для связи соединения пользователя и необходимого нам параметра
+// create a ws-server. all your users will connect to it
+$ws_worker = new Worker("websocket://0.0.0.0:8000");
+
+// storage of user-connection link
 $users = [];
 
-// создаём ws-сервер, к которому будут подключаться все наши пользователи
-$ws_worker = new Worker("websocket://0.0.0.0:8000");
-// создаём обработчик, который будет выполняться при запуске ws-сервера
+$ws_worker->onConnect = function($connection) use (&$users)
+{
+    $connection->onWebSocketConnect = function($connection) use (&$users)
+    {
+        // put get-parameter into $users collection when a new user is connected
+        // you can set any parameter on site page. for example client.html: ws = new WebSocket("ws://127.0.0.1:8000/?user=tester01");
+        $users[$_GET['user']] = $connection;
+        // or you can use another parameter for user identification, for example $_COOKIE['PHPSESSID']
+    };
+};
+
+$ws_worker->onClose = function($connection) use(&$users)
+{
+    // unset parameter when user is disconnected
+    $user = array_search($connection, $users);
+    unset($users[$user]);
+};
+
+// it starts once when you start server.php:
 $ws_worker->onWorkerStart = function() use (&$users)
 {
-    // создаём локальный tcp-сервер, чтобы отправлять на него сообщения из кода нашего сайта
+    // create a local tcp-server. it will receive messages from your site code (for example from send.php)
     $inner_tcp_worker = new Worker("tcp://127.0.0.1:1234");
-    // создаём обработчик сообщений, который будет срабатывать,
-    // когда на локальный tcp-сокет приходит сообщение
+    // create a handler that will be called when a local tcp-socket receives a message (for example from send.php)
     $inner_tcp_worker->onMessage = function($connection, $data) use (&$users) {
-        $data = json_decode($data);
-        // отправляем сообщение пользователю по userId
+        // you have to use for $data json_decode because send.php uses json_encode
+        $data = json_decode($data); // but you can use another protocol for send data send.php to local tcp-server
+        // send a message to the user by userId
         if (isset($users[$data->user])) {
             $webconnection = $users[$data->user];
             $webconnection->send($data->message);
         }
     };
     $inner_tcp_worker->listen();
-};
-
-$ws_worker->onConnect = function($connection) use (&$users)
-{
-    $connection->onWebSocketConnect = function($connection) use (&$users)
-    {
-        // при подключении нового пользователя сохраняем get-параметр, который же сами и передали со страницы сайта
-        $users[$_GET['user']] = $connection;
-        // вместо get-параметра можно также использовать параметр из cookie, например $_COOKIE['PHPSESSID']
-    };
-};
-
-$ws_worker->onClose = function($connection) use(&$users)
-{
-    // удаляем параметр при отключении пользователя
-    $user = array_search($connection, $users);
-    unset($users[$user]);
 };
 
 // Run worker
